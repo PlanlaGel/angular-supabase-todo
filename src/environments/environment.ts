daa7040e1bc2308ce88e67db33134416f86191cf
planlagel <planlagel@gmail.com>
2025-05-25 13:51:53 +0300

    yeni deploy.yml ve diğer düzenlemeler

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+/* export const environment = {
+  production: true,
+  supabaseUrl: '__NG_APP_SUPABASE_URL__', // Yer tutucu
+  supabaseKey: '__NG_APP_SUPABASE_KEY__', // Yer tutucu
+}; */
+
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://gndgvlcvqtxcoaxjxetj.supabase.co', // Supabase Proje URL'niz
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduZGd2bGN2cXR4Y29heGp4ZXRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNTgwNjcsImV4cCI6MjA2MzYzNDA2N30.W7H4ryFLMMVWR21eF1M6TtavgsKhrIDNP1bL2a-JQuw', // Supabase Anon Key'iniz
+};
