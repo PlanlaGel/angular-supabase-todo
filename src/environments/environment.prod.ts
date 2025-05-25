5d99fa25573a284ec477b7a8b620151697c71580
planlagel <planlagel@gmail.com>
2025-05-25 12:51:20 +0300

    environment prod eklendi

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  supabaseUrl: '__NG_APP_SUPABASE_URL__', // Yer tutucu
+  supabaseKey: '__NG_APP_SUPABASE_KEY__', // Yer tutucu
+};
